59979324c63048adf295cc68ad78b0489c4858b2
vega <ival@pdx.edu>
2020-03-03 03:26:33 -0800

    90% worked out

--- src/Textfile.java
+++ src/Textfile.java
@@ -13,13 +13,26 @@ public class Textfile {
         //FileWriter fout = new FileWriter("test.txt");
         //fout.write("Testing Scanner 10 12.2 one true two false");
         //fout.close();
+        String filename;
+        System.out.println("Enter file name. Do NOT include the .txt file extension");
+        System.out.println("For now, just enter \"test\"");
+        Scanner user_in = new Scanner(System.in);
+        filename = user_in.nextLine();
+        filename = filename + ".txt";
 
         FileReader fin;
-        fin = new FileReader("test.txt");
+        //fin = new FileReader("test.txt");
+        fin = new FileReader(filename);
+
 
         Scanner src = new Scanner(fin);
-        src.useDelimiter(":");
+        src.useDelimiter(":|\n");
+        // the argument means ":" and newline aka "\\n" act as delimiters in the file
+        // without these, spaces are the default delimiter
+        // also periods?
 
+
+        /*
         while (src.hasNext()) {
             if (src.hasNextInt()) {
                 i = src.nextInt();
@@ -38,8 +51,22 @@ public class Textfile {
                 str = src.next();
                 System.out.println("String: " + str);
             }
-        }
 
+         */
+        while (src.hasNext()) {
+
+            i = src.nextInt();
+            System.out.println("int: " + i);
+            d = src.nextDouble();
+            System.out.println("double: " + d);
+            b = src.nextBoolean();
+            System.out.println("boolean: " + b);
+            str = src.next();
+            System.out.println("String: " + str);
+
+            System.out.println(String.format("%d %.2f %b %s", i, d, b, str));
+            // the .2 inbetween % and f will force display only 2 digits after decimal
+        }
         fin.close();
     }
 }
