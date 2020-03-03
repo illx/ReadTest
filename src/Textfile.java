6c19fb3a9bf082db5fbfc752c36562e76df8984d
Ivan Leung <vega@Ivans-iMac.local>
2020-03-03 02:55:08 -0800

    Initial commit

---
+++ src/Textfile.java
@@ -0,0 +1,45 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Textfile {
+    public static void main(String args[]) throws IOException {
+
+        int i;
+        double d;
+        boolean b;
+        String str;
+
+        //FileWriter fout = new FileWriter("test.txt");
+        //fout.write("Testing Scanner 10 12.2 one true two false");
+        //fout.close();
+
+        FileReader fin;
+        fin = new FileReader("test.txt");
+
+        Scanner src = new Scanner(fin);
+        src.useDelimiter(":");
+
+        while (src.hasNext()) {
+            if (src.hasNextInt()) {
+                i = src.nextInt();
+                System.out.println("int: " + i);
+            }
+
+            else if (src.hasNextDouble()) {
+                d = src.nextDouble();
+                System.out.println("double: " + d);
+            }
+            else if (src.hasNextBoolean()) {
+                b = src.nextBoolean();
+                System.out.println("boolean: " + b);
+            }
+            else {
+                str = src.next();
+                System.out.println("String: " + str);
+            }
+        }
+
+        fin.close();
+    }
+}
