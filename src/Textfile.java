62dd5fa1a2768f05b19dba2884e15b49259440c5
vega <ival@pdx.edu>
2020-03-04 12:41:26 -0800

    95% worked out

--- src/Textfile.java
+++ src/Textfile.java
@@ -5,68 +5,53 @@ import java.util.Scanner;
 public class Textfile {
     public static void main(String args[]) throws IOException {
 
+        // Right now, this only works if you put the external data file one level above the "src" folder. I guess the
+        // top level of your project directory?
+
+
         int i;
         double d;
         boolean b;
         String str;
 
-        //FileWriter fout = new FileWriter("test.txt");
-        //fout.write("Testing Scanner 10 12.2 one true two false");
-        //fout.close();
         String filename;
         System.out.println("Enter file name. Do NOT include the .txt file extension");
         System.out.println("For now, just enter \"test\"");
-        Scanner user_in = new Scanner(System.in);
-        filename = user_in.nextLine();
+        // There's no special reason to omit the ".txt" extension. I just wanted to test how to concatenate strings
+
+        Scanner user_input = new Scanner(System.in);
+
+        filename = user_input.nextLine();
         filename = filename + ".txt";
+        // Java version of strcat
 
-        FileReader fin;
-        //fin = new FileReader("test.txt");
-        fin = new FileReader(filename);
+        // This gets the actual file
+        FileReader external_file;
+        external_file = new FileReader(filename);
 
 
-        Scanner src = new Scanner(fin);
-        src.useDelimiter(":|\n");
+        Scanner file_input = new Scanner(external_file);
+        file_input.useDelimiter(":|\n");
         // the argument means ":" and newline aka "\\n" act as delimiters in the file
         // without these, spaces are the default delimiter
         // also periods?
 
 
-        /*
-        while (src.hasNext()) {
-            if (src.hasNextInt()) {
-                i = src.nextInt();
-                System.out.println("int: " + i);
-            }
 
-            else if (src.hasNextDouble()) {
-                d = src.nextDouble();
-                System.out.println("double: " + d);
-            }
-            else if (src.hasNextBoolean()) {
-                b = src.nextBoolean();
-                System.out.println("boolean: " + b);
-            }
-            else {
-                str = src.next();
-                System.out.println("String: " + str);
-            }
+        while (file_input.hasNext()) {
 
-         */
-        while (src.hasNext()) {
-
-            i = src.nextInt();
+            i = file_input.nextInt();
             System.out.println("int: " + i);
-            d = src.nextDouble();
+            d = file_input.nextDouble();
             System.out.println("double: " + d);
-            b = src.nextBoolean();
+            b = file_input.nextBoolean();
             System.out.println("boolean: " + b);
-            str = src.next();
+            str = file_input.next();
             System.out.println("String: " + str);
 
             System.out.println(String.format("%d %.2f %b %s", i, d, b, str));
             // the .2 inbetween % and f will force display only 2 digits after decimal
         }
-        fin.close();
+        external_file.close();
     }
 }
